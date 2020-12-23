0954e020fe19c5fe6992008985e076ecb832e534
Raja-mishra1 <rajamishra0702@gmail.com>
2020-12-23 11:23:28 +0530

    refactored initial code

--- process_payment.py
+++ process_payment.py
@@ -11,8 +11,13 @@ class Validate:
         self.security_code = security_code
         self.amount = amount
 
-    def validate_credit_card_number(self, card_number):
-        card_number = list(card_number)
+    def validate_credit_card_number(self,credit_card_number):
+        """[Validates credit card number used in transaction by using Luhn algorithm]
+
+        Returns:
+            [bool]: [Returns True if card number is valid otherwise False]
+        """
+        card_number = list(credit_card_number)
         check_digit = card_number.pop()
         card_number.reverse()
         processed_digits = []
@@ -34,10 +39,15 @@ class Validate:
         else:
             return False
 
-    def validate_data(self,credit_card_number,card_holder,expiration_date,security_code,amount):
-        if credit_card_number:
-            if type(credit_card_number) == str:
-                resp = self.validate_credit_card_number(credit_card_number)
+    def validate_data(self):
+        """[Validates credit card details]
+
+        Returns:
+            [Response]: [Returns response if the card details are invalid]
+        """
+        if self.credit_card_number:
+            if type(self.credit_card_number) == str:
+                resp = self.validate_credit_card_number(self.credit_card_number)
                 if resp == False:
                     return Response("Bad request", status=400)
             else:
@@ -46,28 +56,28 @@ class Validate:
             return Response("Bad request", status=400)
         
 
-        if card_holder:
-            if  type(card_holder) != str:
+        if self.card_holder:
+            if  type(self.card_holder) != str:
                 return Response("Bad request", status=400)
         else:
             return Response("Bad request", status=400)
             
-        if expiration_date:
-            if type(expiration_date) == datetime:
+        if self.expiration_date:
+            if type(self.expiration_date) == datetime:
                 current_date = datetime.today()
-                if expiration_date < current_date:
+                if self.expiration_date < current_date:
                     return Response("Bad request", status=400)
             else:
                 return Response("Bad request", status=400)
         else:
             return Response("Bad request", status=400)
         
-        if security_code:
-            if len(security_code) != 3 or type(security_code) != str:
+        if self.security_code:
+            if len(self.security_code) != 3 or type(self.security_code) != str:
                 return Response("Bad request", status=400)
 
-        if amount:
-            if type(amount) != float and amount >= 0:
+        if self.amount:
+            if type(self.amount) != float and self.amount >= 0:
                 return Response("Bad request", status=400)
         else:
             return Response("Bad request", status=400)
@@ -75,8 +85,20 @@ class Validate:
 
 
 def process_payment(credit_card_number, card_holder, expiration_date, security_code, amount):
+    """[Validates credit card related data and passes payment related to payment gateway]
+
+    Args:
+        credit_card_number ([str]): [credit card number used in the transaction]
+        card_holder ([str]): [Name of credit card holder]
+        expiration_date ([datetime]): [Expiration date of credit card]
+        security_code ([str]): [Security code of credit card]
+        amount ([float]): [Amount involved in the transaction]
+
+    Returns:
+        [response]: [Response from payment gateway]
+    """
     v = Validate(credit_card_number, card_holder, expiration_date, security_code, amount)
-    v.validate_data(credit_card_number, card_holder, expiration_date, security_code, amount)
+    v.validate_data()
     try:
         if amount <= 20:
             resp = CheapPaymentGateway(credit_card_number, card_holder, expiration_date, security_code, amount)
